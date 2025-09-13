5de737d86fe0871af0bd078b4765cd7523570aae
Abanoub Aziz <abanoubsamy2341@gmail.com>
2025-09-13 12:14:16 +0300

    edit: configuring environment variables correctly according to usecase, adding .mocharc.json, adding new constants

--- apis/config/client.ts
+++ apis/config/client.ts
@@ -1,16 +1,6 @@
 import { PrismaClient } from "@prisma/client"
-import dotenv from "dotenv";
 import path from 'node:path'
-const envFile = process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
-dotenv.config({ path: path.resolve(process.cwd(), envFile) })
 
-
-const db = process.env.DATABASE_URL;
-
-if (!db) {
-  throw new Error("Database URL is not defined in environment variables.");
-}
-
-const prisma = new PrismaClient({ datasources: { db: { url: db } } });
+const prisma = new PrismaClient();
 
 export default prisma;
